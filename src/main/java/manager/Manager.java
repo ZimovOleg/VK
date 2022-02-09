3cdb9dcef6d39ac13f4b06bcbb8844e306c3e3c4
ZimovOleg <zimov.oleg@gmail.com>
2022-02-09 09:42:18 +0400

    added Manager

---
+++ src/main/java/manager/Manager.java
@@ -0,0 +1,11 @@
+package manager;
+
+import domain.Post;
+
+public class Manager {
+
+    private Post [] search (int ownerId, String domain, String query, boolean ownersOnly, int count, int offset){
+        return null;
+    }
+    public void delete (int ownerId, int postId) {}
+}
